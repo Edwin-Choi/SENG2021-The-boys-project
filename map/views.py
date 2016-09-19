0d256db67cac4280ec579ffacdc7d3cc98fb71e8
JHXSMatthew <68638023@qq.com>
2016-09-19 07:47:50 -0700

    ASSET API working now

--- map/views.py
+++ map/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Position
+import urllib.request
+
+
+
+class AppURLopener(urllib.request.FancyURLopener):
+    version = "Mozilla/5.0"
 
 # Create your views here.
 def index(request):
     if request.method == 'POST':
-        return HttpResponse("something interesting?")
+        name = request.POST.get('pac-input')
+        print(name);
+        opener = AppURLopener()
+        response = opener.open('http://api.nestoria.co.uk/api?encoding=json&action=search_listings&country=uk&pretty=1&listing_type=buy&place_name='+ name)
+        print(response);
+        return HttpResponse(response)
     else:
         position = Position.objects.all();
-        name = request.GET.get('pac-input');
-        print(name);
     return render(request, 'map.html', {'pois': position})
 
--- seng2021/settings.py
+++ seng2021/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "geoposition",
+    "urllib",
 ]
 
 MIDDLEWARE_CLASSES = [
