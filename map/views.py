8628980bdd57c7fb882290c527af1b89a5803b61
JHXSMatthew <68638023@qq.com>
2016-09-19 12:54:08 -0700

    dragging stuff

--- map/views.py
+++ map/views.py
@@ -15,10 +15,18 @@ def index(request):
         keyword = request.POST.get('keyword')
         page = 1;
         page = request.POST.get('page');
+        if name is not None:
+            controlStr = '&place_name='+ name; 
+        else:
+            controlStr = '&south_west='+ request.POST.get('south_west') + '&north_east=' + request.POST.get('north_east') ;
+        
         if page is None:
             page = 1;
-        url = 'http://api.nestoria.co.uk/api?encoding=json&action=search_listings&number_of_results=50&country=uk&pretty=1&listing_type=buy&place_name='+ name + '&page=' + str(page);
-        print(name + " " + str(page));
+
+        url = 'http://api.nestoria.co.uk/api?encoding=json&action=search_listings&number_of_results=50&country=uk&pretty=1&listing_type=buy' + controlStr + '&page=' + str(page);
+        
+        print(controlStr + " " + str(page))
+        
         opener = AppURLopener()
         response = opener.open(url)
         return HttpResponse(response)
