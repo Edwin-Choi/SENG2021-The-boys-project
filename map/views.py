f76efd4426815fcc1100b22a4535533b93d7736d
Alva <alvahuynh@gmail.com>
2016-09-20 04:15:34 +1000

    help

--- map/views.py
+++ map/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Position
 import urllib.request
-
+import json
 
 
 class AppURLopener(urllib.request.FancyURLopener):
@@ -12,10 +12,15 @@ class AppURLopener(urllib.request.FancyURLopener):
 def index(request):
     if request.method == 'POST':
         name = request.POST.get('pac-input')
-        print(name);
+        keyword = request.POST.get('keyword')
+        page = 1;
+        page = request.POST.get('page');
+        if page is None:
+            page = 1;
+        url = 'http://api.nestoria.co.uk/api?encoding=json&action=search_listings&number_of_results=50&country=uk&pretty=1&listing_type=buy&place_name='+ name + '&page=' + str(page);
+        print(name + " " + str(page));
         opener = AppURLopener()
-        response = opener.open('http://api.nestoria.co.uk/api?encoding=json&action=search_listings&country=uk&pretty=1&listing_type=buy&place_name='+ name)
-        print(response);
+        response = opener.open(url)
         return HttpResponse(response)
     else:
         position = Position.objects.all();
