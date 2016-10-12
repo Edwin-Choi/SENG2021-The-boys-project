3a4e3c95a89cb18a95896e797829f16bbeea47f4
JHXSMatthew <68638023@qq.com>
2016-10-12 18:32:28 +1100

    so javascript can code in OOP way
    massive work need to be done, clean up and so on.
    I am pretty confident now, start working tomorrow!

---
+++ map/static/js/Marker.js
@@ -0,0 +1,24 @@
+/*
+  The JS calss represent a marker
+  warrper google map's marker and fixed asset info.
+
+  Fields
+   - googleMarker  -> the marker of googleMap API
+   - 
+
+
+*/
+
+
+class Marker{
+	constructor(googleMarker, assetInfo){
+		this.googleMarker = googleMarker;
+		this,assetInfo = assetInfo;
+	}
+	
+
+	update(){
+		
+	}
+
+}
