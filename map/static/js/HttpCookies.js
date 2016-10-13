f36caacdabd4d8d4d40ef731ac749dea134d16e9
JHXSMatthew <68638023@qq.com>
2016-10-13 17:32:26 +1100

    clean up codes, add more js files

---
+++ map/static/js/HttpCookies.js
@@ -0,0 +1,75 @@
+
+
+
+/*
+  the general HTTP get function . to send a http GET request
+*/
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+/*
+  get cookies, to pass security stuff
+*/
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+
+/*
+   HTTP post requset to get actually asset info
+   callback in use to update markers.
+*/
+function sendHTTPPost(control,fit){
+    var csrftoken = getCookie('csrftoken');
+    var http = new XMLHttpRequest();
+    http.open("POST", "", true);
+    http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    http.setRequestHeader("X-CSRFToken",csrftoken);
+    getExtraSearchWords();
+    var params = control + getExtraSearchWords();
+    var doModify = getExtraSearchWords() != lastWords;
+    lastWords = getExtraSearchWords();
+    if(doModify){
+        clearMarkers();
+    }
+    http.send(params)
+    http.onload = function() {
+        //JSON objects here
+        var allInfo = JSON.parse(http.responseText);
+        //console.log(http.responseText);
+        if(allInfo.response.application_response_code != 200){
+            if(allInfo.page < allInfo.total_pages){
+                var nextPage = parseInt(allInfo.page) + 1;
+                http.send(params + "&page=" + nextPage);
+            }
+            var responseArray = allInfo.response.listings;
+            for(var i = 0 ; i < responseArray.length ; i++){
+                //console.log(i);
+                addMarker(responseArray[i].latitude,responseArray[i].longitude,responseArray[i].title,responseArray[i]);
+            }
+          
+        }
+        if(fit == 1){
+            map.fitBounds(bounds);
+        }
+        
+    }
+   
+
+}
--- map/static/js/Marker.js
+++ map/static/js/Marker.js
@@ -21,4 +21,107 @@ class Marker{
 		
 	}
 
+	getPosition(){
+		return this.googleMarker.getPosition();
+	}
+
+}
+
+
+/*
+   add marker function to add market into global cache array
+*/
+function addMarker(lat, lng, title, info) {
+        var position = new google.maps.LatLng(lat, lng);
+        if(!shouldAddMarker(position)){
+            console.log("shoud not log this!")
+            return;
+        }
+        var marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: title
+        });
+
+        markers.push(new Marker(marker,info));
+
+        bounds.extend(position);
+        if(info != null){
+            //Info windows
+            var contentString = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">'+ info.title +'</h1>'+
+            '<img src='+ info.img_url +' style="width:304px;height:228px;">'+
+            '<div id="bodyContent">'+
+            '<p>'+ info.summary + ' </p>'+
+            '<p> Price Starting: '+ info.price + " "+ info.price_currency + ' </p>'+
+            '<p> Property Type: '+ info.property_type + ' </p>'+
+            '<p> Bedroom number: '+ info.bedroom_number + ' </p>'+
+            '<p> bathroom number: '+ info.bathroom_number + ' </p>'+
+            '<p><a href= '+ info.lister_url +'>'+
+            'Click for details</a> '+
+            '</p>'+
+            '</div>'+
+            '</div>';
+            var infowindow = new google.maps.InfoWindow({
+            content: contentString
+            });
+        }
+         
+        marker.addListener('click', function() {
+            infowindow.open(map, marker);
+        });
+}
+
+
+/*
+  fetch data from server by place name
+*/
+function updateMarkers(name,fit){
+   sendHTTPPost("pac-input=" + name, fit);
+   onUpdate();
+}
+
+
+/*
+   fetch data from servr by bound
+*/
+function updateMarkersByBound(bound){
+    sendHTTPPost("&south_west=" + bound.getSouthWest().lat() + "," + bound.getSouthWest().lng() 
+                 + "&north_east=" + bound.getNorthEast().lat() + "," + bound.getNorthEast().lng(),0);
+    onUpdate();
+}
+
+/*
+  apply a filter to current markers.
+
+*/
+function onUpdate(){
+	markers.forEach(function(marker){
+		marker.update();
+	});
+}
+
+function shouldAddMarker(obj){
+    markers.forEach(function(marker) {
+        if(marker.getPosition() == obj){
+            return false;
+        }
+       
+    });
+    return true;
+               
+}
+
+
+/*
+   remove all markers
+*/
+function clearMarkers(){
+    //clear out old markers
+    markers.forEach(function(marker) {
+        marker.setMap(null);
+    });
+    markers = [];
 }
